11ae3e4a488db761665b68c743edd7cd446bd852
Davyalvs <davyalvez890@gmail.com>
2025-01-17 18:41:07 -0300

    criação de usuario e pessoa e telas de cadastropessoa e menu

--- src/Entidades/Usuario.java
+++ src/Entidades/Usuario.java
@@ -1,13 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Entidades;
-
 /**
  *
- * @author samuelcds
+ * @author Davy
  */
-class Usuario {
-    
+public class Usuario extends Pessoa {
+    private String curso;
+    private boolean cadastrado;
+    private String usuario;
+    private String senha;
+    private Livro[] livrosEmprestados;
+
+    // Construtor
+    public Usuario(int id, String nome, String genero, String senha, String cpf, String curso, boolean cadastrado, String usuario, Livro[] livrosEmprestados) {
+        super(id, nome, genero, senha, cpf);
+        this.curso = curso;
+        this.cadastrado = cadastrado;
+        this.usuario = usuario;
+        this.livrosEmprestados = livrosEmprestados;
+    }
+
+    // Métodos Getters e Setters
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public boolean isCadastrado() {
+        return cadastrado;
+    }
+
+    public void setCadastrado(boolean cadastrado) {
+        this.cadastrado = cadastrado;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+
+    public Livro[] getLivros() {
+        return livrosEmprestados;
+    }
+
+    public void setLivros(Livro[] livrosEmprestados) {
+        this.livrosEmprestados = livrosEmprestados;
+    }
 }
